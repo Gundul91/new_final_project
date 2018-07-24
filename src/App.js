726e8eede66f6d1a4df1ffefcd777d8bdb3479f4
marco <gundul1@libero.it>
2018-07-24 10:43:27 +0200

    add fetch request to foursquare

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import MyMap from './MyMap.js'
 
 class App extends Component {
   state = {
-    center: { lat: 41.898611, lng: 12.476873 },
+    center: { lat: 41.89297, lng: 12.5108 },
     zoom: 14
   }
+  getPlaces() {
+    fetch('https://api.foursquare.com/v2/venues/search?near=Rome&radius=150&categoryId=4d4b7105d754a06374d81259&client_id=LC0GY54VBUJOVC5RURAESLSK1TK3YPNGYXGVK3BWDGGTAHEX&client_secret=JFZ3ZCNVU4RDDH1PUV3KHKS5PLLJSQ5RVPQZ4AJNLMZAB1MV&v=20170901')
+    .then(res => res.json())
+    .then(res => console.log(res))
+  }
   render() {
+    this.getPlaces()
     return (
       <div className="App">
         <MyMap
