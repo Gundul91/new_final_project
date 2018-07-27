42fb24af5a1ceb60a92f63dfebe0c87e40fedebc
marco <gundul1@libero.it>
2018-07-27 12:59:23 +0200

    add some styles

--- src/PlaceList.js
+++ src/PlaceList.js
@@ -15,8 +15,8 @@ class PlaceList extends Component {
     const{listOpen, headerTitle} = this.state
     return(
       <div className="placeBar">
-        <input type="text" placeholder="Search" onChange={(e) => this.props.getPlaces('https://api.foursquare.com/v2/venues/search?near=Rome&radius=150&categoryId=4d4b7105d754a06374d81259&client_id=LC0GY54VBUJOVC5RURAESLSK1TK3YPNGYXGVK3BWDGGTAHEX&client_secret=JFZ3ZCNVU4RDDH1PUV3KHKS5PLLJSQ5RVPQZ4AJNLMZAB1MV&v=20170901&query=' + e.target.value)}/>
-        <h2>Locations</h2>
+        <input className="searchBar" type="text" placeholder="Search" onChange={(e) => this.props.getPlaces('https://api.foursquare.com/v2/venues/search?near=Rome&radius=150&categoryId=4d4b7105d754a06374d81259&client_id=LC0GY54VBUJOVC5RURAESLSK1TK3YPNGYXGVK3BWDGGTAHEX&client_secret=JFZ3ZCNVU4RDDH1PUV3KHKS5PLLJSQ5RVPQZ4AJNLMZAB1MV&v=20170901&query=' + e.target.value)}/>
+        <h1>Locations</h1>
         <ul className="placeList">
         {this.props.places.map(item => (
           <li className="location" key={item.id} id={item.id} onClick={this.props.onListClick}>{item.name}</li>
